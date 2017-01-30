abf76e675ab098d70eb5955a3390a8a7704bdc1b
Luke Sontrop <luke@bluesaltlabs.com>
2017-01-30 12:46:21 -0800

    Updates to routes file (splits up API and Web routes)

--- routes/web.php
+++ routes/web.php
@@ -1,60 +1,8 @@
 <?php
 
-// API and Back End
-//Route::group(['middleware' => 'auth'], function() {
-    Route::group(['prefix' => 'api/v1'], function() {
-        Route::group(['prefix' => 'docs'], function() {
-            Route::get('/', 'DocsController@index');
-            Route::get('search', 'DocsController@searchIndex');
-    
-            Route::group(['prefix' => 'admin'], function() {
-                Route::get('/', 'DocsController@adminIndex');
-            });
-            Route::group(['prefix' => 'appointment'], function() {
-                Route::get('/', 'DocsController@appointmentIndex');
-            });
-            Route::group(['prefix' => 'calendar'], function() {
-                Route::get('/', 'DocsController@calendarIndex');
-            });
-            Route::group(['prefix' => 'config'], function() {
-                Route::get('/', 'DocsController@configIndex');
-            });
-            Route::group(['prefix' => 'member'], function() {
-                Route::get('/', 'DocsController@memberIndex');
-            });
-            Route::group(['prefix' => 'user'], function() {
-                Route::get('/', 'DocsController@userIndex');
-            });
-        });
-    
-        Route::group(['prefix' => 'user'], function() {
-            class User extends Illuminate\Database\Eloquent\Model {  }
-            Route::get('/', 'UserController@getAll');
-            Route::get('get/{id}', 'UserController@get');
-            Route::post('create', 'UserController@create');
-            Route::delete('delete/{id}', 'UserController@delete');
-        });
-    
-        Route::group(['prefix' => 'appointment'], function() {
-            class Appointment extends Illuminate\Database\Eloquent\Model {  }
-            Route::get('/', 'AppointmentController@getAll');
-            Route::get('/getByDateRange/', 'AppointmentController@getByDateRange');
-            Route::get('get/{id}', 'AppointmentController@get');
-            Route::post('create', 'AppointmentController@create');
-            Route::post('schedule', 'AppointmentController@schedule');
-    
-            //Route::get('getTest/{startDate}/{endDate}', 'AppointmentController@getTest'); // debug
-        });
-    });
-    
-    Route::group(['prefix' => 'calendar'], function() {
-        Route::get('/embed/{year}/{month}/{day}', 'CalendarController@getCalendar');
-    });
-
-//});
-
 // Front End
 
+//Route::group(['middleware' => 'auth'], function() {
 //Route::group(['middleware' => 'admin-auth'], function() {
     Route::group(['prefix' => 'admin'], function() {
         Route::get('/', 'AdminController@index');
