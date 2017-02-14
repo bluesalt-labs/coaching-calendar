b91f5f091e72f402dc7228b25d4bbff8258209ab
Luke Sontrop <luke@bluesaltlabs.com>
2017-02-14 15:55:42 -0800

    Adds what I think is something important but I don't remember why.

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\App;
 
 class TestController extends Controller
 {
+
+    public function __construct(){
+        $this->middleware('auth:web');
+    }
+
     public function index(Request $request) {
         return view('test.index');
     }
